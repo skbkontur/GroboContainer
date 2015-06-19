fe5135034d57976298484baf09b786296cdf8a53
Alexander Chicherskiy <chicherskiy@skbkontur.ru>
2015-06-20 00:30:44 +0500

    use ConcurrentDictionary instead of Dictionary with lock for AbstractionConfigurationCollection

---
+++ GroboContainer/GroboContainer/Impl/ConcurrentDictionaryExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace GroboContainer.Impl
+{
+    public static class ConcurrentDictionaryExtensions
+    {
+        public static bool TryAddOrUpdate<TKey, TValue>(
+            this ConcurrentDictionary<TKey, TValue> dict,
+            TKey key, TValue value, Func<TValue, bool> updateFilter)
+        {
+            TValue curValue;
+            bool found;
+            while ((found = dict.TryGetValue(key, out curValue)) && updateFilter(curValue) || !found)
+            {
+                if ((!found && dict.TryAdd(key, value)) || (found && dict.TryUpdate(key, value, curValue)))
+                    return true;
+            }
+            return false;
+        }
+    }
+}

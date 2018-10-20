c633b627bfc328239950611581cd884522a7799c
yupliner <yupliner@skbkontur.ru>
2018-10-20 10:08:02 +0500

    Rename TypeArrays to TypeArray

--- GroboContainer/Impl/TypeArray.cs
+++ GroboContainer/Impl/TypeArray.cs
@@ -2,22 +2,22 @@ using System;
 
 namespace GroboContainer.Impl
 {
-    internal static class TypeArrays<T>
+    internal static class TypeArray<T>
     {
         public static readonly Type[] Instance = {typeof(T)};
     }
 
-    internal static class TypeArrays<T1, T2>
+    internal static class TypeArray<T1, T2>
     {
         public static readonly Type[] Instance = {typeof(T1), typeof(T2)};
     }
 
-    internal static class TypeArrays<T1, T2, T3>
+    internal static class TypeArray<T1, T2, T3>
     {
         public static readonly Type[] Instance = {typeof(T1), typeof(T2), typeof(T3)};
     }
 
-    internal static class TypeArrays<T1, T2, T3, T4>
+    internal static class TypeArray<T1, T2, T3, T4>
     {
         public static readonly Type[] Instance = {typeof(T1), typeof(T2), typeof(T3), typeof(T4)};
     }

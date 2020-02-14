661fd06824a126dd61f2106a0c50e5b395f178a7
avk <Andrew.Kostousov@gmail.com>
2020-02-14 10:41:35 +0500

    + WhenUseTypeConfigurationBindsTwoInterfacesToSingleImplType_ContainerShouldCreateOnlyOneInstanceOfImplType test

--- GroboContainer.Tests/FunctionalTests/ReuseImplementationsWithExplicitUseTypeConfigurationTest.cs
+++ GroboContainer.Tests/FunctionalTests/ReuseImplementationsWithExplicitUseTypeConfigurationTest.cs
@@ -1,21 +1,33 @@
-﻿using NUnit.Framework;
+using NUnit.Framework;
 
 namespace GroboContainer.Tests.FunctionalTests
 {
-    public class ReuseImplementationsWithExplicitUseTypeConfigurationTest : ContainerTestBase
+    public class UseTypeTests : ContainerTestBase
     {
-        public interface IInterface
-        {
-        }
-
         [Test]
-        public void TestBug()
+        public void ReuseImplementationsWithExplicitUseTypeConfigurationTest()
         {
             container.Configurator.ForAbstraction<IInterface>().UseType<Impl>();
             Assert.That(container.Get<IInterface>(), Is.SameAs(container.Get<Impl>()));
         }
 
-        public class Impl : IInterface
+        [Test]
+        public void WhenUseTypeConfigurationBindsTwoInterfacesToSingleImplType_ContainerShouldCreateOnlyOneInstanceOfImplType()
+        {
+            container.Configurator.ForAbstraction<IInterface>().UseType<Impl>();
+            container.Configurator.ForAbstraction<IAnotherInterface>().UseType<Impl>();
+            Assert.That(container.Get<IInterface>(), Is.SameAs(container.Get<IAnotherInterface>()));
+        }
+
+        public interface IInterface
+        {
+        }
+
+        public interface IAnotherInterface
+        {
+        }
+
+        public class Impl : IInterface, IAnotherInterface
         {
         }
     }
